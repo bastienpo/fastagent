583b3b48d6479cddcbf289db6404d761344a124f
Bastien <bastien.pouessel@epita.fr>
2024-11-11 12:52:34 -0800

    chore(frontend): eslint config with react and typescript

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,20 +2,32 @@ import globals from "globals"
 import pluginJs from "@eslint/js"
 import tseslint from "typescript-eslint"
 import pluginReact from "eslint-plugin-react"
+import pluginReactHooks from "eslint-plugin-react-hooks"
 import pluginPrettier from "eslint-config-prettier"
 import pluginOxlint from "eslint-plugin-oxlint"
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
+    { ignores: ["dist", "pnpm-lock.yaml", "coverage"] }, // global ignores
+    pluginJs.configs.recommended, // javascript predefined config
+    ...tseslint.configs.recommended, // typescript predefined config
     {
         files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-        ignores: ["dist"],
-        languageOptions: { globals: globals.browser },
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+        },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+        plugins: {
+            reactHooks: pluginReactHooks,
+        },
     },
-    pluginReact.configs.flat.recommended,
     pluginReact.configs.flat["jsx-runtime"],
-    pluginOxlint.configs["flat/recommended"],
-    pluginPrettier,
+    pluginPrettier, // prettier shareable config
+    pluginOxlint.configs["flat/recommended"], // oxlint shareable config
 ]
